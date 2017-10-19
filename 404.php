ededf3363fb0a632cdb216ed1f6ca3d7f00e02e6
NerdOfCode <NerdOfCode@users.noreply.github.com>
2017-10-19 07:57:19 -0500

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php
+$ip=$_SERVER['REMOTE_ADDR'];
+echo "The file you tried to access is protected and you dont have permission to view it... <br>";
+echo "Your IP has been reported: $ip";
+
+
+
+
+?>
