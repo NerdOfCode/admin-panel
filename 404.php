1855e104d448213bf8824302c2e4ae6eb343c904
nerdofcode <admin@nerdofcode.com>
2017-10-20 18:56:28 +0000

    Fixed

--- 404.php
+++ 404.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 if($_SESSION['status']=="1"){
-	header("Location: /options.php");
+	header("Location: /options/php");
 	$_SESSION['logged_in']="1";
 }
 $ip=$_SERVER['REMOTE_ADDR'];
-echo "The file you tried to access is protected and you dont have permission to view it... <br>";
-echo "Your IP has been reported: <b>$ip<b>";
+echo "The file you have tried to access is top secret and unfortuantely you do not have the clearance to access it...";
+echo "Your IP has been reported: <b>$ip</b>";
 
 ?>
