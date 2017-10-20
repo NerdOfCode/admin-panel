7c7b2e4b9cdf9b56c7347fbd13eb4f03f6c3d346
nerdofcode <admin@nerdofcode.com>
2017-10-20 22:39:18 +0000

    New formatting

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if($_SESSION['status']=="1"){
   </head>
   <body>
     <h1 style="text-align:center;">Admin Panel</h1><hr>
-    <form name="form" id="form" action="" method="post" >
+    <form name="index" id="index" action="" method="post">
       Username:&ensp;<input type="text" name="UID" id="UID" required><br><br>
       Password:&ensp;<input type="password" name="passwd" id="passwd" required><br><br>
       <input type="submit" value="Submit" onClick=""">
@@ -24,12 +24,12 @@ if($_SESSION['status']=="1"){
 <script>
 $(document).ready(function(){
 	$('#form').submit(function(){
-		$('#UID').fadeOut();
-		$('#passwd').fadeOut();
+		$('#UID').fadeOut(500);
+		$('#passwd').fadeOut(500);
 })
 });
 </script>
-<p class="footer">By: <a href="https://github.com/NerdOfCode" target="_blank"><b>NerdOf</b>Code</a>, <a href="https://github.com/NerdOfLinux" target="_blank"><b>NerdOf</b>Linux</a></p>    
+<p class="footer">By: <a href="https://github.com/NerdOfCode" target="_blank"><b>NerdOf</b>Code</a>, <a href="https://github.com/NerdOfLinux" target="_blank"><b>NerdOf</b>Linux</a> | <a href="https://github.com/NerdOfCode/admin-panel/blob/master/LICENSE">License</a></p>    
   </body>
   
 <?php
@@ -38,7 +38,7 @@ $user_name = $_POST['UID'];
 $user_password = $_POST['passwd'];
 $db = mysqli_connect('localhost',$user,$pass,$database) or die("Error connecting to MYSQL");
 $query = "SELECT password FROM $table WHERE name = '$user_name'";
-mysqli_query($db, $query) or die("Unable to access MYSQL");
+mysqli_query($db, $query) or die("<p class=\"text-align:center;\">Unable to access MYSQL</p>");
 $result = mysqli_query($db, $query);
 $row = mysqli_fetch_array($result);
 $password=$row['password'];
@@ -47,7 +47,7 @@ if(password_verify($_POST['passwd'], $password)){
         header("Location: /options.php");
         die();
 }else if(! $_SESSION['status']){
-	echo "<p class=\"false\">Please re-enter creds.</p>";
+	echo "<p class=\"false\" style=\"color:red;text-align:center;\">Please re-enter creds.</p>";
 }
 mysqli_close($db);
 
